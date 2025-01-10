c2cb0b0acc96442af54f9ef182bd8639431f757b
Wazeer Almoliky <wazeeralmoliky@gmail.com>
2025-01-10 14:35:10 +0300

    Add New Color

--- lib/utilities/constants/app_color.dart
+++ lib/utilities/constants/app_color.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const primary=Color.fromARGB(255, 36, 99, 38);
   static const secondary=Color.fromARGB(255, 192, 164, 4);
+  static const secondary2=Color.fromARGB(255, 113, 97, 6);
 }
