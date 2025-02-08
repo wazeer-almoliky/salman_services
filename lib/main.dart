d3a0bb1b03c297b4d87e196e3c70ba341fcc96b7
Wazeer Almoliky <wazeeralmoliky@gmail.com>
2025-02-09 00:58:21 +0300

    change title of app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'خدمات سلمان',
+      title: 'خدمات عامة',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColor.primary),
